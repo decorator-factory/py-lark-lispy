92a866fbc4102e87b8405c5d6ba06f970593de00
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-08-02 12:35:16 +0300

    Create threadsafe logic and more entities (entity stubs)

--- pylarklispy/entities.py
+++ pylarklispy/entities.py
@@ -1,14 +1,24 @@
-from typing import Callable, Mapping
+from typing import Callable, Mapping, Tuple, Literal
 """
 This module contains the classes that represent all the language
 entities like S-Expr, Integer etc.
+
+An entity is an internal representation of a value or a computation.
 """
 
 
 Runtime = Mapping[str, "Entity"]
+EvaluationStatus = Tuple[Literal["full", "partial"], "Entity"]
 
 
 class Entity:
+    @property
+    def is_threadsafe(self):
+        """An entity is threadsafe if its `compute` method does not
+        depend on the runtime
+        """
+        return True
+
     def fmap(self, f: Callable[["Entity"], "Entity"]) -> "Entity":
         return self
 
@@ -16,12 +26,32 @@ class Entity:
         return self
 
     def evaluate(self, runtime: Runtime) -> "Entity":
+        """Run `compute` an entity until it's no longer
+        reducable"""
         state = self
         while True:
             # compute until the entity is final
             next_state = state.compute(runtime)
             if next_state is state:
                 return state
+            state = next_state
+
+    def evaluate_while_threadsafe(self, runtime: Runtime) -> EvaluationStatus:
+        """Like `evaluate`, but stops as soon as computing the next
+        step is not threadsafe.
+
+        If computataion was full, return ("full", <entity>)
+
+        If computataion was partial, return ("partial", <entity>)
+        """
+        state = self
+        while True:
+            if not state.is_threadsafe:
+                return ("partial", state)
+            # compute until the entity is final
+            next_state = state.compute(runtime)
+            if next_state is state:
+                return ("full", state)
             state = next_state
 
 
@@ -41,7 +71,7 @@ class String(Entity):
         self.s = s
 
     def __str__(self):
-        return str(self.s)
+        return repr(self.s)
 
     def __repr__(self):
         return f"<String {self.s!r}>"
@@ -67,3 +97,75 @@ class Quoted(Entity):
 
     def __repr__(self):
         return f"<Quoted {self.e!r}>"
+
+
+class SExpr(Entity):
+    def __init__(self, *es: Entity):
+        self.es = es
+
+    is_threadsafe = False
+
+    def compute(self, runtime: Runtime) -> Entity:
+        raise NotImplementedError # TODO
+
+    def __str__(self):
+        return "(" + " ".join(map(str, self.es)) + ")"
+
+    def __repr__(self):
+        return f"<SExpr {self.es}>"
+
+
+class Vector(Entity):
+    def __init__(self, *es: Entity):
+        self.es = es
+
+    @property
+    def is_threadsafe(self):
+        return all(e.is_threadsafe for e in self.es)
+
+    def compute(self, runtime: Runtime) -> Entity:
+        raise NotImplementedError # TODO
+
+    def __str__(self):
+        return "[" + " ".join(map(str, self.es)) + "]"
+
+    def __repr__(self):
+        return f"<Vector {self.es}>"
+
+
+class Name(Entity):
+    is_threadsafe = False
+
+    def __init__(self, identifier: str):
+        self.identifier = identifier
+
+    def compute(self, runtime: Runtime) -> Entity:
+        return runtime[self.identifier]
+
+    def __str__(self):
+        return self.identifier
+
+    def __repr__(self):
+        return "<Name {self.identifier}>"
+
+
+class SigilString(Entity):
+    def __init__(self, sigil: str, string: str):
+        self.sigil = sigil
+        self.string = string
+
+    @property
+    def sigil_function_name(self) -> str:
+        return f"sigil<{self.sigil}>"
+
+    def compute(self, _runtime) -> Entity:
+        return SExpr(
+            Name(self.sigil_function_name),
+            String(self.string)
+        )
+
+    def __str__(self):
+        return f"~{self.sigil}{self.string!r}"
+
+    def __repr__(self):
+        return "<Sigil {self.sigil} {self.string!r}>"
