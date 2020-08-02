0b42272e5bc50188deee6279e5f4d5a973c7aff4
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-08-02 13:48:04 +0300

    use lark.Transformer to generate the AST

--- pylarklispy/parser.py
+++ pylarklispy/parser.py
@@ -1,5 +1,10 @@
+import json
+from os import stat
 from os.path import join, dirname, realpath
+
 import lark
+
+from . import entities
 
 DIR = dirname(realpath(__file__))
 GRAMMAR_FILENAME = join(DIR, "grammar.lark")
@@ -8,3 +13,45 @@ with open(GRAMMAR_FILENAME) as grammar_file:
     grammar = grammar_file.read()
 
 parser = lark.Lark(grammar, parser="lalr")
+
+@lark.v_args(inline=True)
+class Transformer(lark.Transformer):
+    @staticmethod
+    def ESCAPED_STRING(token):
+        return json.loads(str(token))
+
+    @staticmethod
+    def start(*e: entities.Entity):
+        return e
+
+    @staticmethod
+    def integer(token):
+        return entities.Integer(int(token))
+
+    @staticmethod
+    def string(s):
+        return entities.String(s)
+
+    @staticmethod
+    def atom(token):
+        return entities.Atom(str(token))
+
+    @staticmethod
+    def sigil_string(sigil_token, string):
+        return entities.SigilString(str(sigil_token), string)
+
+    @staticmethod
+    def name(token):
+        return entities.Name(str(token))
+
+    @staticmethod
+    def s_expr(*es: entities.Entity):
+        return entities.SExpr(*es)
+
+    @staticmethod
+    def vector(*es: entities.Entity):
+        return entities.Vector(*es)
+
+    @staticmethod
+    def quoted(subentity):
+        return entities.Quoted(subentity)
